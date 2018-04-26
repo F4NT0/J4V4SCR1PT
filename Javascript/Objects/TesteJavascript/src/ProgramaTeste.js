7584d5369b92ff4297313b099a4faba9cf310ac8
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2018-04-26 17:24:56 -0300

    Alterado o ProgramaTeste

--- Javascript/Objects/TesteJavascript/src/ProgramaTeste.js
+++ Javascript/Objects/TesteJavascript/src/ProgramaTeste.js
@@ -2,20 +2,13 @@
 function calculos(valor1,valor2){
     this.valor1 = valor1;
     this.valor2 = valor2;
-    this.getValor1 = getV1;
-    this.getValor2 = getV2;
     this.soma = sum;
     this.subtracao = sub;
     this.multiplicacao = mult;
     this.divisao = div;
     this.restoDivisao = rest;
 }
-function getV1(){
-    return this.valor1;
-}
-function getV2(){
-    return this.valor2;
-}
+
 function sum(){
     return this.valor1 + this.valor2;
 }
@@ -47,8 +40,6 @@ function rest(){
 let valor1 = 8;
 let valor2 = 7;
 let novo = new calculos(valor1,valor2);
-document.write(novo.getValor1());
-document.write("\n" +  novo.getValor2());
 document.write("\n" + novo.soma());
 document.write("\n" + novo.subtracao());
 document.write("\n" + novo.multiplicacao());
